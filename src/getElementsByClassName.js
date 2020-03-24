55c111b72b70a6d386a41c3abf1a9fc78a216023
jgenso1690 <jimenagensollen@gmail.com>
2020-03-23 23:22:05 -0400

    getclasname

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -7,20 +7,20 @@
 
  // document.body - does it have children -yes? ???
 var getElementsByClassName = function(className, element) {
-  var elementList;
+  var elementList = [];
   element = element || document.body
   var target = className;
 
  // does it have the className?(nodeX)
  if (element.className === className){
     //if yes, push to result array
-   elementList = [element];
+   elementList.push(element);
  }
   //if childcount > 0 (nodeX)
   if (element.childElementCount > 0){
     //loop through nodeX children and apply to each getElement(nodexchil[i])
     for (var i = 0; i < element.children.length; i++) {
-     elementList.push(getElementsByClassName(target, element.children[i]));
+     elementList.concat(getElementsByClassName(target, element.children[i]));
     }
   }
   // return array with div elements
