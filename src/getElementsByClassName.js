24b953c47c9789ecfaacd0c9fb1f67e396783bac
jgenso1690 <jimenagensollen@gmail.com>
2020-03-24 00:37:37 -0400

    getelement

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -12,7 +12,8 @@ var getElementsByClassName = function(className, element) {
   var target = className;
 
  // does it have the className?(nodeX)
- if (element.className === className){
+ console.log(element.className)
+ if (element.classList.contains(className)){
     //if yes, push to result array
    elementList.push(element);
  }
@@ -20,10 +21,11 @@ var getElementsByClassName = function(className, element) {
   if (element.childElementCount > 0){
     //loop through nodeX children and apply to each getElement(nodexchil[i])
     for (var i = 0; i < element.children.length; i++) {
-     elementList.concat(getElementsByClassName(target, element.children[i]));
+     elementList = elementList.concat(getElementsByClassName(target, element.children[i]));
     }
   }
   // return array with div elements
+  console.log(elementList);
   return elementList;
 }
 
